a2f03d958978149ce06277d3b1ff13c5f657d247
AliHussain <alihussain08950@gmail.com>
2024-09-05 13:59:21 +0300

    Sign in functionality fixed and bug within the authentication service fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
+import { useState } from 'react';
 
 // Importing Homepage component
 import Homepage from './components/Homepage/Homepage.jsx';
@@ -19,13 +20,16 @@ import PageNotFound from './components/ErrorComponents/PageNotFound/PageNotFound
 import About from './components/FooterComponents/About/About.jsx';
 import Contact from './components/FooterComponents/Contact/Contact.jsx';
 
+import authenticationServices from './services/authenticationServices.js';
+
 function App() {
+  const [user, setUser] = useState(authenticationServices.getUser());
   return (
     <>
       <Navbar/>
         <Routes>
           <Route path="/" element={<Homepage/>}/>
-          <Route path="/signin" element={<SignIn/>}/>
+          <Route path="/signin" element={<SignIn setUser={setUser}/>}/>
           <Route path="/signup" element={<SignUp/>}/>
           <Route path="/about" element={<About/>}/>
           <Route path="/contact" element={<Contact/>}/>
--- src/components/Authentication/SignIn/SignIn.jsx
+++ src/components/Authentication/SignIn/SignIn.jsx
@@ -1,17 +1,40 @@
 import './SignIn.css';
-export default function SignIn() {
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import authenticationServices from '../../../services/authenticationServices';
+
+export default function SignIn({setUser}) {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  function handleFormData(event) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+    await authenticationServices.signin({
+      username: formData.username,
+      password: formData.password
+    });
+    const user = await authenticationServices.getUser();
+    setUser(user);
+    navigate(`/user/${user.id}/item`);
+  }
+  
+
     return (
       <>
       <div className='formContainer'>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <h1>SignIn</h1>
           <div className='usernameContainer'>
             <label htmlFor='username'>username </label>
-            <input type="text" name='username' id='password' placeholder='username'/>
+            <input type="text" name='username' id='password' placeholder='username' onChange={handleFormData} value={formData.username}/>
           </div>
           <div className='passwordContainer'>
             <label htmlFor='password'>password </label>
-            <input type="password" name='password' id='password' placeholder='password' />
+            <input type="password" name='password' id='password' placeholder='password' onChange={handleFormData} value={formData.password}/>
           </div>  
           <div className='submitButton'>
             <button type='submit'>Submit</button>
